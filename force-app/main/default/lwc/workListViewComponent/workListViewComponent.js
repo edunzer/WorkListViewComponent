01c85f49b95038e1c4a6a4b47977a89f49ce645b
Ethan Dunzer <ethandunzer@yahoo.com>
2024-08-20 09:16:12 -0700

    v1.0.0

---
+++ force-app/main/default/lwc/workListViewComponent/workListViewComponent.js
@@ -0,0 +1,37 @@
+import { LightningElement, wire } from 'lwc';
+import getWorkRecords from '@salesforce/apex/WorkListViewController.getWorkRecords';
+
+const columns = [
+    { label: 'Work Id', fieldName: 'workUrl', type: 'url', 
+      typeAttributes: { label: { fieldName: 'Name' }}},
+    { label: 'Subject', fieldName: 'agf__Subject__c' },
+    { label: 'Status', fieldName: 'agf__Status__c' },
+    { label: 'Scheduled Build', fieldName: 'scheduledBuildUrl', type: 'url', 
+      typeAttributes: { label: { fieldName: 'scheduledBuildName' }}},
+    { label: 'Assigned To', fieldName: 'assigneeUrl', type: 'url', 
+      typeAttributes: { label: { fieldName: 'assigneeName' }}}
+];
+
+export default class WorkListViewComponent extends LightningElement {
+    columns = columns;
+    workRecords;
+
+    @wire(getWorkRecords)
+    wiredWorkRecords({ error, data }) {
+        if (data) {
+            this.workRecords = data.map(record => {
+                return {
+                    ...record,
+                    workUrl: `/ideaexchange/s/adm-work/${record.Id}`,
+                    scheduledBuildUrl: record.agf__Scheduled_Build__c ? `/ideaexchange/s/adm-build/${record.agf__Scheduled_Build__c}` : null,
+                    scheduledBuildName: record.agf__Scheduled_Build__r ? record.agf__Scheduled_Build__r.Name : 'N/A',
+                    assigneeUrl: record.agf__Assignee__c ? `/ideaexchange/s/profile/${record.agf__Assignee__c}` : null,
+                    assigneeName: record.agf__Assignee__r ? record.agf__Assignee__r.Name : 'N/A'
+                };
+            });
+        } else if (error) {
+            this.workRecords = undefined;
+            console.error('Error fetching work records:', error);
+        }
+    }
+}
